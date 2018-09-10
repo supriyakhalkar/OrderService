11fa26b06621c35e70644b2a60537905d22fde93
supriyakhalkar <supriya.khalkar@synerzip.com>
2018-09-10 18:42:13 +0530

    OrderService

---
+++ src/main/java/com/synerzip/training/OrderController.java
@@ -0,0 +1,31 @@
+package com.synerzip.training;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class OrderController {
+
+	@Value("${user.discountFactor}")
+	int discountFactor;
+	
+	int cnt = 0;
+	
+	@RequestMapping(value = "/discount/{userName}")
+	public ResponseEntity<String> checkForDistcount() {
+		cnt ++;
+		String result = null;
+		if(cnt % discountFactor == 0) {
+			result = "Discount is applied"; 
+		}else {
+			result = "Discount is not Applied";
+		}
+		System.out.println("This is request number " + cnt);
+		return new ResponseEntity<String>(result,HttpStatus.OK);
+	}
+}
